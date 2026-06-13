b2e74990ffaaef85229428224c51e85e8005f2a4
laggykiller <chaudominic2@gmail.com>
2026-06-13 22:33:59 +0800

    PWA

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,33 @@
 module.exports = {
-  publicPath: '/sushiro-vue/'
+  publicPath: '/sushiro-vue/',
+  pwa: {
+    name: 'Sushiro-vue',
+    themeColor: '#000000',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    iconPaths: {
+      faviconSVG: 'public/assets/icon/sushi.svg',
+      favicon32: null,
+      favicon16: null,
+      appleTouchIcon: null,
+      maskIcon: null,
+      msTileImage: null,
+    },
+    manifestOptions: {
+      icons: [
+        {
+          src: 'assets/icon/sushi.svg',
+          sizes: 'any',
+          type: 'image/svg+xml',
+          purpose: 'maskable any',
+        },
+      ]
+    },
+    workboxPluginMode: 'GenerateSW',
+    workboxOptions: {
+      skipWaiting: true,
+      clientsClaim: true,
+    }
+  }
 }
